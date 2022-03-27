ec4bf59307a6d71545333f9f1757a5e1d9bc994f
eatspaint <cef2010@gmail.com>
2022-03-26 18:58:00 -0700

    fix favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,10 @@ const Home: NextPage = () => {
       <Head>
         <title>Guitar Practice</title>
         <meta name="description" content="Guitar excercises" />
-        <link rel="icon" href="/guitar.svg" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎸</text></svg>"
+        ></link>
       </Head>
 
       <Container>
